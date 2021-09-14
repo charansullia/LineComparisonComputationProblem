717ecc94bc734c56173bb2c80b8e1673a0b2c800
charasullia <charanrajr713@gmail.com>
2021-09-14 22:45:16 +0530

    [charansullia]added comparison of Line

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -8,15 +8,19 @@ namespace LineComparison
         {
             LineComparisonComputation Line1 = new LineComparisonComputation(8, 9, 6, 8);
             double Length1 = Line1.LineComputation();
-            LineComparisonComputation Line2 = new LineComparisonComputation(10, 4, 7, 9);
+            LineComparisonComputation Line2 = new LineComparisonComputation(8, 9, 6, 8);
             double Length2 = Line2.LineComputation();
-            if(Length1==Length2)
+            if(Length1>Length2)
+            {
+                Console.WriteLine("Line1 is greater than Line2");
+            }
+            else if(Length1==Length2)
             {
                 Console.WriteLine("Lines are equal");
             }
             else
             {
-                Console.WriteLine("Lines are not equal");
+                Console.WriteLine("Line2 is greater than Line1");
             }
         }
     }
