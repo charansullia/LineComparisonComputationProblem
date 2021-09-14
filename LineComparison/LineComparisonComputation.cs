467fe1bcfb7efe03aeb43b442bd8063b027a2b8a
charasullia <charanrajr713@gmail.com>
2021-09-14 22:21:24 +0530

    [charansullia]added Equality of Line

--- LineComparison/LineComparisonComputation.cs
+++ LineComparison/LineComparisonComputation.cs
@@ -7,18 +7,23 @@ namespace LineComparison
     class LineComparisonComputation
     {
         int x1, x2, y1, y2;
-        public  LineComparisonComputation(int x1, int x2, int y1, int y2)
+        public LineComparisonComputation(int x1, int x2, int y1, int y2)
         {
             this.x1 = x1;
-            Console.WriteLine("value of x1 is:"+x1);
+            Console.WriteLine("value of x1 is:" + x1);
             this.x2 = x2;
-            Console.WriteLine("value of x2 is:"+x2);
+            Console.WriteLine("value of x2 is:" + x2);
             this.y1 = y1;
-            Console.WriteLine("value of y1 is:"+y1);
+            Console.WriteLine("value of y1 is:" + y1);
             this.y2 = y2;
-            Console.WriteLine("value of y2 is:"+y2);
-            double Length = Math.Sqrt((x2 - x1) + (y2 - y1));
-            Console.WriteLine("Length of line is:" +Length);
+            Console.WriteLine("value of y2 is:" + y2);
         }
+        public double LineComputation()
+        {
+            double Length = Math.Sqrt((x2 - x1) + (y2 - y1));
+            Console.WriteLine("Length of line is:" + Length);
+            return Length;
+        }
+        
     }
 }
--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -6,7 +6,18 @@ namespace LineComparison
     {
         static void Main(string[] args)
         {
-            LineComparisonComputation obj = new LineComparisonComputation(2, 3, 6, 8);
+            LineComparisonComputation Line1 = new LineComparisonComputation(8, 9, 6, 8);
+            double Length1 = Line1.LineComputation();
+            LineComparisonComputation Line2 = new LineComparisonComputation(10, 4, 7, 9);
+            double Length2 = Line2.LineComputation();
+            if(Length1==Length2)
+            {
+                Console.WriteLine("Lines are equal");
+            }
+            else
+            {
+                Console.WriteLine("Lines are not equal");
+            }
         }
     }
 }
